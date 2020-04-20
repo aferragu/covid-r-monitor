34caac1f0c2db3ac03abf0483a3f7382b67ad28b
Andres Ferragut <andres.ferragut@gmail.com>
2020-04-20 16:28:09 -0300

    removed dependencies

--- init.R
+++ init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("shinydashboard","xts","MCMCpack","coarseDataTools","EpiEstim")
+my_packages = c("shinydashboard","xts","EpiEstim")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
