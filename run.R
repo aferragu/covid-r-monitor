5a9bcda694f653f2d63cccfffc8c1374626a789a
Andres Ferragut <andres.ferragut@gmail.com>
2020-04-20 17:31:51 -0300

    con pais

--- run.R
+++ run.R
@@ -12,18 +12,26 @@ ui <- dashboardPage(
   dashboardSidebar(),
   dashboardBody(
     fluidRow(
-     box(plotOutput("plot_incidence", height = 250)),
-     box(plotOutput("plot_estimR", height = 250))
+      infoBox("Uruguay")
+    )
+    fluidRow(
+     box(title = "Casos registrados", plotOutput("plot_incidence", height = 250)),
+     box(title = "Estimacion de tasa R",plotOutput("plot_estimR", height = 250))
    ),
    fluidRow(
      uiOutput("choose_country")
   )
+  fluidRow(
+   box(title = "Casos registrados", plotOutput("plot_incidence_2", height = 250)),
+   box(title = "Estimacion de tasa R",plotOutput("plot_estimR_2", height = 250))
+ ),
+
  )
 )
 
 
 
-server <- function(input, output) {
+server <- function(input, output,session) {
 
   download.file("https://covid.ourworldindata.org/data/ecdc/full_data.csv", "full_data.csv")
   data <- read.csv(file = 'full_data.csv')
@@ -52,10 +60,10 @@ server <- function(input, output) {
 
   output$plot_estimR <- renderPlot({
     plot(serieR)
-    lines(serieRl)
-    lines(serieRu)
-    abline(h=1,col="red",lwd=2)
-    abline(h=0,lwd=2)
+#    lines(serieRl)
+#    lines(serieRu)
+#    abline(h=1,col="red",lwd=2)
+#    abline(h=0,lwd=2)
 
   })
 
@@ -64,6 +72,35 @@ server <- function(input, output) {
       selectInput("pais", "Pais", unique(data[,"location"]))
   })
 
+  datos_country <- data[data$location == 'Uruguay',]
+  times_country <- as.Date(datos_country[,"date"])
+  serie_country <- xts(datos_country[,c("new_cases")],order.by=times_country)
+
+  res_country <- estimate_R(incid = datos_uy[,"new_cases"],
+                  method = "non_parametric_si",
+                  config = make_config(list(si_distr = discrete_si_distr)))
+
+  times2_country=tail(times_country,-7)
+  serieR_country <- xts(res_country$R[,c("Median(R)")],order.by=times2_country)
+  serieRl_country <- xts(res_country$R[,c("Quantile.0.025(R)")],order.by=times2_country)
+  serieRu_country <- xts(res_country$R[,c("Quantile.0.975(R)")],order.by=times2_country)
+
+  output$plot_incidence_country <- renderPlot({
+    plot(serie_country)
+  })
+
+  output$plot_estimR_country <- renderPlot({
+    plot(serieR_country)
+#    lines(serieRl)
+#    lines(serieRu)
+#    abline(h=1,col="red",lwd=2)
+#    abline(h=0,lwd=2)
+
+  })
+
+  output$result <- renderText({
+      paste("Pais: ", input$choose_country)
+    })
 
 }
 
