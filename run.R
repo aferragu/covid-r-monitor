1906ac8e40c5cc200fd4ba9a3786e0bcf4eef00c
Andres Ferragut <andres.ferragut@gmail.com>
2020-04-20 15:06:42 -0300

    changed UI

--- run.R
+++ run.R
@@ -6,12 +6,20 @@ options(shiny.host = '0.0.0.0')
 options(shiny.port = as.numeric(Sys.getenv('PORT')))
 
 ui <- dashboardPage(
-  dashboardHeader(),
+  dashboardHeader(title = "COVID R Monitor"),
   dashboardSidebar(),
-  dashboardBody()
+  dashboardBody(
+    fluidRow(
+          box(
+            width = 8, status = "info", solidHeader = TRUE,
+            title = "Popularity by package (last 5 min)",
+            
+          ),
+        )
+
+  )
 )
 
 server <- function(input, output) { }
 
 shinyApp(ui, server)
-
