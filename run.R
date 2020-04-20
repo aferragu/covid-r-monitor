710c09fd80e3479903a3fc7c4f6016d147fa65dd
Andres Ferragut <andres.ferragut@gmail.com>
2020-04-20 15:43:03 -0300

    removed controls

--- run.R
+++ run.R
@@ -12,11 +12,6 @@ ui <- dashboardPage(
   dashboardBody(
     fluidRow(
      box(plotOutput("plot1", height = 250)),
-
-     box(
-       title = "Controls",
-       sliderInput("slider", "Number of observations:", 1, 100, 50)
-     )
    )
  )
 )
