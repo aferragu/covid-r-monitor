23dc6d29a2981d62ffa8d9971ceb827891d241a7
Andres Ferragut <andres.ferragut@gmail.com>
2020-08-06 15:33:42 -0300

    Reorganized code into appropriate functions. Not finished

--- server.R
+++ server.R
@@ -5,42 +5,55 @@ library(plotly)
 
 shinyServer(function(input, output, session) {
 
-      download.file("https://covid.ourworldindata.org/data/ecdc/full_data.csv", "full_data.csv")
-      data <- read.csv(file = 'full_data.csv')
-      datos_uy <- data[data$location == 'Uruguay',]
-      times <- as.Date(datos_uy[,"date"])
-      incidencia <- datos_uy[,"new_cases"]
+    #####Auxiliary functions
 
-      datos_incidencia_uy <- data.frame(Tiempo=times,Incidencia=incidencia)
+    #Get complete data_frame from server
+    get_data <- function(location="https://covid.ourworldindata.org/data/ecdc/full_data.csv") {
+        download.file(location, "full_data.csv")
+        data <- read.csv(file = 'full_data.csv')
+        return(data)
+    }
 
-      output$plot_incidence <- renderPlotly({
-        plot_ly(
-            data = datos_incidencia_uy,
+    #Filter Time and Incidence data for a given country
+    filter_data <- function(data,country) {
+        datos_country <- data[data$location == country,]
+        times <- as.Date(datos_country[,"date"])
+        incidencia <- datos_country[,c("new_cases")]
+        datos_incidencia_country <- data.frame(Tiempo=times,Incidencia=incidencia)
+        return(datos_incidencia_country)
+    }
+
+    #Estimate R from incidence and parameters
+    estimate_R_country <- function(datos,window=7,mean_covid_si=3.95,sd_covid_si=4.75,delta_si=30) {
+
+        discrete_si_distr <- discr_si(seq(0, delta_si), mean_covid_si, sd_covid_si)
+        res <- estimate_R(incid = pmax(datos$Incidencia,0),
+                        method = "non_parametric_si",
+                        config = make_config(list(si_distr = discrete_si_distr)))
+
+        shortened_times <- tail(datos$Tiempo,-window)
+        datos_R_country <- data.frame(Tiempo=shortened_times,R=res$R[,c("Median(R)")],Rl=res$R[,"Quantile.0.025(R)"],Ru=res$R[,"Quantile.0.975(R)"])
+        return(datos_R_country)
+    }
+
+    #Create Plotly Barplot for incidence
+    plotly_incidence <- function(datos) {
+        fig<- plot_ly(
+            data = datos,
             x = ~Tiempo,
             y = ~Incidencia,
             name = "Nuevos casos diarios",
             type = "bar"
           )
-      })
+          return(fig)
+    }
 
-      #estimo el R
-      mean_covid_si<-3.95
-      sd_covid_si <- 4.75
-      delta_si<-30
-      discrete_si_distr <- discr_si(seq(0, delta_si), mean_covid_si, sd_covid_si)
-
-      res <- estimate_R(incid = pmax(incidencia,0),
-                      method = "non_parametric_si",
-                      config = make_config(list(si_distr = discrete_si_distr)))
-
-      times2=tail(times,-7)
-      datos_R_uy <- data.frame(Tiempo=times2,R=res$R[,c("Median(R)")],Rl=res$R[,"Quantile.0.025(R)"],Ru=res$R[,"Quantile.0.975(R)"])
-
-
-      output$plot_estimR <- renderPlotly({
+    #Create Plotly Barplot for R
+    plotly_R <- function(datos_R_country){
+        n=nrow(datos_R_country)
 
         fig <- plot_ly(
-                data=datos_R_uy,
+                data=datos_R_country,
                 x = ~Tiempo,
                 y = ~R,
                 type = 'scatter',
@@ -66,11 +79,8 @@ shinyServer(function(input, output, session) {
             showlegend = FALSE,
             hoverinfo='skip',
             name = 'Limite inferior')
-
-
-
         fig <- fig %>% add_trace(
-                y = rep(1,length(times2)),
+                y = rep(1,n),
                 type = 'scatter',
                 mode = 'lines',
                 showlegend = FALSE,
@@ -82,132 +92,22 @@ shinyServer(function(input, output, session) {
             title = "R estimado",
             yaxis = list(title="R",range=c(0,3.5),hoverformat = '.2f')
         )
-      })
+    }
 
-        output$downloadData <- downloadHandler(
-            filename = function() {
-              paste("estimacion_R_Uruguay-", Sys.Date(), ".csv", sep="")
-            },
-            content = function(file) {
-              write.csv(res$R, file)
-            }
-        )
 
-      output$uruguay <- renderInfoBox({
-          infoBox(
-            "R actual Uruguay",
-            tail(res$R[,c("Median(R)")], n=1)
-          )
-      })
+    ######Main logic and rendering functions
 
-      output$uruguay_ci_lower <- renderInfoBox({
-          infoBox(
-            "Cuantil 0.025",
-            tail(res$R[,c("Quantile.0.025(R)")], n=1)
-          )
-      })
+    #Download data
+    data <- get_data()
 
-      output$uruguay_ci_upper <- renderInfoBox({
-          infoBox(
-            "Cuantil 0.975",
-            tail(res$R[,c("Quantile.0.975(R)")], n=1)
-          )
-      })
+    #Filter Uruguay
+    datos_incidencia_uy <- filter_data(data,"Uruguay")
 
-      output$choose_country <- renderUI({
-          selectInput("pais", "Pais", unique(data[,"location"]))
-      })
-
-      actualizar_serie_pais <- reactive( {
-          datos_country <- data[data$location == input$pais,]
-          times <- as.Date(datos_country[,"date"])
-          incidencia <- datos_country[,c("new_cases")]
-          datos_incidencia_country <- data.frame(Tiempo=times,Incidencia=incidencia)
-          datos_incidencia_country
-          }
-        )
-
-      output$plot_incidence_country <- renderPlotly({
-          plot_ly(
-              data = actualizar_serie_pais(),
-              x = ~Tiempo,
-              y = ~Incidencia,
-              name = "Nuevos casos diarios",
-              type = "bar"
-            )
-
-      })
-
-      actualizar_calculo_pais <- reactive( {
-            datos_country <- data[data$location == input$pais,]
-            times <- as.Date(datos_country[,"date"])
-
-            #cambio los NA por 0
-            datos_country[is.na(datos_country[,"new_cases"]),"new_cases"] <- 0
-
-            incidencia <- datos_country[,c("new_cases")]
-
-            res <- estimate_R(incid = pmax(incidencia,0),
-                            method = "non_parametric_si",
-                            config = make_config(list(si_distr = discrete_si_distr)))
-
-            times2 <- tail(times,-7)
-            datos_R_country <- data.frame(Tiempo=times2,R=res$R[,c("Median(R)")],Rl=res$R[,"Quantile.0.025(R)"],Ru=res$R[,"Quantile.0.975(R)"])
-          }
-        )
-
-      output$plot_estimR_country <- renderPlotly({
-
-          data = actualizar_calculo_pais()
-          n=nrow(data)
-
-          fig <- plot_ly(
-                  data=data,
-                  x = ~Tiempo,
-                  y = ~R,
-                  type = 'scatter',
-                  mode = 'lines',
-                  line = list(color='rgb(31, 119, 180)'),
-                  name = 'R estimado')
-          fig <- fig %>% add_trace(
-                      x = ~Tiempo,
-                      y = ~Ru,
-                      type = 'scatter',
-                      mode = 'lines',
-                      line = list(color = 'transparent'),
-                      showlegend = FALSE,
-                      hoverinfo='skip',
-                      name = 'Limite superior')
-          fig <- fig %>% add_trace(
-              y = ~Rl,
-              type = 'scatter',
-              mode = 'lines',
-              fill = 'tonexty',
-              fillcolor='rgba(31, 119, 180,0.2)',
-              line = list(color = 'transparent'),
-              showlegend = FALSE,
-              hoverinfo='skip',
-              name = 'Limite inferior')
+    output$plot_incidence <- renderPlotly({
+      plotly_incidence(datos_incidencia_uy)
+    })
 
 
 
-          fig <- fig %>% add_trace(
-                  y = rep(1,n),
-                  type = 'scatter',
-                  mode = 'lines',
-                  showlegend = FALSE,
-                  line = list(color='rgb(127,0,0)',width=1,dash="dash"),
-                  hoverinfo='skip',
-                  name = 'Referencia')
 
-          fig <- fig %>% layout(
-              title = "R estimado",
-              yaxis = list(title="R",range=c(0,3.5),hoverformat = '.2f')
-          )
-      })
-
-      output$result <- renderText({
-          paste("Pais: ", input$choose_country)
-        })
-
-})
+}
