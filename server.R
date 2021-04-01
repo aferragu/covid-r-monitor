f0e0f9aac1bfcf1c52a5f0cf69f8b88f87478924
Andres Ferragut <andres.ferragut@gmail.com>
2021-04-01 15:12:17 -0300

    Changed column of GUIAD data to account for late arriving cases.

--- server.R
+++ server.R
@@ -45,11 +45,11 @@ shinyServer(function(input, output, session) {
     process_data_guiad <- function(datos, W=7, W2=3) {
 
         fecha <- datos[,"fecha"]
-        incidencia <- datos[,"cantCasosNuevos"]
+        incidencia <- datos[,"cantCasosNuevosCALC"]
         incidencia_ma <- stats::filter(as.numeric(incidencia), rep(1,W)/W, sides=1)
         activos <- datos[,"cantPersonasConInfeccionEnCurso"]
-        ratio_test <- as.numeric(datos[,"cantCasosNuevos"])/as.numeric(datos[,"cantTest"])*100
-        ratio_test_suavizado_num <- stats::filter(as.numeric(datos[,"cantCasosNuevos"]), rep(1,W2), sides=1)
+        ratio_test <- as.numeric(datos[,"cantCasosNuevosCALC"])/as.numeric(datos[,"cantTest"])*100
+        ratio_test_suavizado_num <- stats::filter(as.numeric(datos[,"cantCasosNuevosCALC"]), rep(1,W2), sides=1)
         ratio_test_suavizado_den <- stats::filter(as.numeric(datos[,"cantTest"]), rep(1,W2), sides=1)
         ratio_test_suavizado <- ratio_test_suavizado_num/ratio_test_suavizado_den*100
         return(data.frame(Tiempo = fecha, Incidencia = incidencia, MediaMovil = incidencia_ma, Activos = activos, RatioTest = ratio_test, RatioTestSuavizado = ratio_test_suavizado))
@@ -181,7 +181,7 @@ shinyServer(function(input, output, session) {
     guiad<- get_data_guiad()
 
     data_guiad <- reactive(process_data_guiad(guiad,W=input$window_ma,W2=input$window_ratio))
-    
+
     #Filter Uruguay
     datos_incidencia_uy <- reactive(data_guiad())
     datos_R_uy <- reactive(estimate_R_country(datos_incidencia_uy(), window=input$window_R,mean_covid_si=input$mean_covid_si,sd_covid_si=input$sd_covid_si))
