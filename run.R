acb87a8b00c6d364eedbea6efa2e3fcbb938cb19
Andres Ferragut <andres.ferragut@gmail.com>
2022-08-15 22:10:26 -0300

    Avoided unnecessary write to disk

--- run.R
+++ run.R
@@ -16,23 +16,20 @@ cache_timeout <- 1800 #half an hour
 ## Functions to get the data
 #Get complete data_frame from server
 get_data <- memoise(function(location="https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/jhu/full_data.csv") {
-    download.file(location, "full_data.csv")
-    data <- read.csv(file = 'full_data.csv')
+    data <- read.csv(location)
     return(data)
 }, cache = cachem::cache_mem(max_age = cache_timeout))
 
 #Get complete data_frame from GUIAD
 get_data_guiad <- memoise(function(location="https://raw.githubusercontent.com/GUIAD-COVID/datos-y-visualizaciones-GUIAD/master/datos/estadisticasUY.csv") {
-    download.file(location, "estadisticasUY.csv")
-    data <- read.csv(file = 'estadisticasUY.csv',na="N/A")
+    data <- read.csv(location,na="N/A")
     data[,"fecha"] <- as.Date(data[,"fecha"],format="%d/%m/%Y")
     data <- data[order(data[,"fecha"]),]
     return(data)
 }, cache = cachem::cache_mem(max_age = cache_timeout))
 
 get_stringency_data <- memoise(function(location="https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_nat_latest.csv") {
-    download.file(location, "stringency.csv")
-    data <- read.csv(file = 'stringency.csv')
+    data <- read.csv(location)
     return(data)
 }, cache = cachem::cache_mem(max_age = cache_timeout))
 
